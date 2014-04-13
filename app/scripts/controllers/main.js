ae051903cabb443cc2a325cc75327b3edfbf7c6e
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-13 12:28:16 +0100

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('myBookmarksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
