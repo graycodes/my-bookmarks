567fa67696d6eda8e7fc41ce001d0fc01d044bed
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-13 18:00:44 +0100

    Listing bookmarks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 angular.module('myBookmarksApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $http) {
+        $http.get('data.json')
+            .then(function(res) {
+                var uniqueCats = ['Comics', 'Dev', 'Entertainment', 'Misc', 'Music', 'News', 'Video'];
+
+                $scope.bookmarks = res.data;
+                $scope.categories = uniqueCats;
+
+                var indexed = [];
+
+                $scope.bookmarksToFilter = function() {
+                    indexed = [];
+                    return $scope.bookmarks;
+                };
+
+                $scope.filterCats = function(bookmark) {
+                    var newCat = indexed.indexOf(bookmark.category) === -1;
+
+                    if (newCat) {
+                        indexed.push(bookmark.category);
+                    }
+                    return newCat;
+                };
+
+              });
+
+      });
