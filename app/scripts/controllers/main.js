f888f7cff921f403d932511e430b8e3d4b97328f
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-15 08:29:50 +0100

    Added error checking for images, removed unused code.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,29 +2,17 @@
 
 angular.module('myBookmarksApp')
     .controller('MainCtrl', function ($scope, $http) {
+
+        $('img').error(function(e) {
+            console.log(e);
+        });
+
         $http.get('data.json')
             .then(function(res) {
                 var uniqueCats = ['Comics', 'Dev', 'Entertainment', 'Misc', 'Music', 'News', 'Video'];
 
                 $scope.bookmarks = res.data;
                 $scope.categories = uniqueCats;
-
-                var indexed = [];
-
-                $scope.bookmarksToFilter = function() {
-                    indexed = [];
-                    return $scope.bookmarks;
-                };
-
-                $scope.filterCats = function(bookmark) {
-                    var newCat = indexed.indexOf(bookmark.category) === -1;
-
-                    if (newCat) {
-                        indexed.push(bookmark.category);
-                    }
-                    return newCat;
-                };
-
               });
 
       });
