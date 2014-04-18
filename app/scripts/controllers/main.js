bc87227544202a54ff0b99de8fefe6a52216638d
Gray MacGregor <gmacg.uk@gmail.com>
2014-04-18 17:00:00 +0100

    Added deleted items toggle

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,16 +3,25 @@
 angular.module('myBookmarksApp')
     .controller('MainCtrl', function ($scope, $http) {
 
-        $('img').error(function(e) {
-            console.log(e);
-        });
-
         $http.get('data.json')
             .then(function(res) {
                 var uniqueCats = ['Comics', 'Dev', 'Entertainment', 'Misc', 'Music', 'News', 'Video'];
 
                 $scope.bookmarks = res.data;
                 $scope.categories = uniqueCats;
-              });
+            });
 
-      });
+        $scope.showItem = function(deleted, allowDeleted) {
+            console.log(deleted, allowDeleted);
+            if (!deleted) {
+                return true;
+            } else if (deleted && allowDeleted) {
+                return true;
+            } else {
+                return true;
+            }
+            return !!(!deleted || allowDeleted);
+        };
+
+
+    });
